272b185b86144b682a286c8c96e2bd2420f540b6
Keith Lockwood <kwlockwo@gmail.com>
2019-11-14 17:02:38 +1100

    Use SSL

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,12 @@
 
 require 'barnes'
 
+ssl_bind '127.0.0.1', '5000', {
+    key: 'private.key',
+    cert: 'certificate.pem',
+    verify_mode: 'none'
+}
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
